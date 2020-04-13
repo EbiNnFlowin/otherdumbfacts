172f41f5ed13a98d7cb2bbb7ccbcaf946b1538d8
dhalimon <Rome@206>
2020-04-13 18:00:52 -0400

    dumbfacts

---
+++ app/src/main/java/com/example/otherdumbfacts/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.otherdumbfacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button
+
+    private TextView factTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
